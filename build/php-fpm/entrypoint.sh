51522b6cf8beba3533b6cc502b11c3b075797760
Sergey Shevchenko <sergey.leonidovich.shevchenko@gmail.com>
2020-05-17 18:54:22 +0300

    Change build and setting for employer-cashbox project.

--- build/php-fpm/entrypoint.sh
+++ build/php-fpm/entrypoint.sh
@@ -22,16 +22,16 @@ echo "MYSQL_CREDENTIAL: ${MYSQL_CREDENTIAL}"
 cp ./.env.dist './.env' && chmod 777 './.env'
 sed -i "s/^APP_ENV=/APP_ENV=${APP_ENV}/" './.env'
 sed -i "s/^APP_SECRET=/APP_SECRET=${RANDOM_STRING}/" './.env'
-sed -i "s,^DATABASE_URL=,DATABASE_URL=mysql://${MYSQL_CREDENTIAL}symfony-starter-mysql:3306/${MYSQL_DATABASE}," './.env'
+sed -i "s,^DATABASE_URL=,DATABASE_URL=mysql://${MYSQL_CREDENTIAL}employer-cashbox-mysql:3306/${MYSQL_DATABASE}," './.env'
 sed -i "s/^LOCALE=/LOCALE=ru_Ru/" './.env'
-sed -i "s/^MYSQL_HOST=/MYSQL_HOST=symfony-starter-mysql/" './.env'
+sed -i "s/^MYSQL_HOST=/MYSQL_HOST=employer-cashbox-mysql/" './.env'
 sed -i "s/^MYSQL_PORT=/MYSQL_PORT=3306/" './.env'
 sed -i "s/^MYSQL_ROOT_PASSWORD=/MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}/" './.env'
 sed -i "s/^MYSQL_DATABASE=/MYSQL_DATABASE=${MYSQL_DATABASE}/" './.env'
 sed -i "s/^MYSQL_USER=/MYSQL_USER=${MYSQL_USER}/" './.env'
 sed -i "s/^MYSQL_PASSWORD=/MYSQL_PASSWORD=${MYSQL_PASSWORD}/" './.env'
 sed -i "s/^NGINX_HOST_HTTP_PORT=/NGINX_HOST_HTTP_PORT=${NGINX_HOST_HTTP_PORT}/" './.env'
-sed -i "s/^WORKSPACE_IP=/WORKSPACE_IP=symfony-starter-mysql/" './.env'
+sed -i "s/^WORKSPACE_IP=/WORKSPACE_IP=employer-cashbox-mysql/" './.env'
 source ./.env
 
 echo "In xdebug.ini OLD ==> $(grep remote_host /usr/local/etc/php/conf.d/xdebug.ini)"
