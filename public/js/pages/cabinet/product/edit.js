ce7dcdf9bc19fdf1ee0143d47a854b133e41dcbd
Sergey Shevchenko <sergey.leonidovich.shevchenko@gmail.com>
2020-05-26 15:44:41 +0300

    [Fix] Add domains.

---
+++ public/js/pages/cabinet/product/edit.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    let url, productId;
+
+    // $('');
+
+    url = `/api/cabinet/robokassa/${productId}`;
+
+    // $.ajax({
+    //     url: url,
+    //     method: 'GET',
+    //     data: {
+    //         product: [productId]
+    //     },
+    //     success: (response, status, XHR) => {
+    //         console.log(`Товар [${productId}: ${productName}] успешно удален.`, response);
+    //         location.reload();
+    //     },
+    //     error: (XHR, status, errorMessage) => {
+    //         console.error('Ошибка получения ответа с сервера', XHR, status, errorMessage);
+    //     }
+    // });
+});
